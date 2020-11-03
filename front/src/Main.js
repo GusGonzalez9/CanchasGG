8a60ff7c2bebdaef26c3e5460960d306f5501a20
LauColombini <colombini.lautaro@gmail.com>
2020-11-03 19:39:20 -0300

    frontViewSingleProduct

--- front/src/Main.js
+++ front/src/Main.js
@@ -2,13 +2,14 @@ import React from "react";
  import { connect } from "react-redux";
 import {Route, Switch,Redirect,component} from 'react-router-dom'
 import Navbar from './components/NavBar/Navbar'
-
+import singleProduct from './components/singleProduct/singleProduct'
 
 class Main extends React.Component {
   render() {
     return (
       <div>
       <Route path="/" component = {Navbar}/>
+      <Route path="/products" component={singleProduct}/> {/* ACA SE RENDERIZARIA EL /products/:id */}
       </div>
     );
   }
---
+++ front/src/components/singleProduct/singleProductStyle.js
@@ -0,0 +1,73 @@
+const style = {
+    everyFather:{
+        display: 'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:20,
+        fontFamily: 'Roboto'
+        
+
+    },
+    fatherAllSingleProduct:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent:'center',
+        marginTop:10
+    },
+    FatherLittleImg:{
+        display:'flex',
+        flexDirection: 'column',
+        maxHeight: 400
+    },
+    littleImg:{
+        maxWidth:100,
+        marginBottom: 50,
+        height:100,
+        
+    },
+    imgPrincipal:{
+        maxWidth: 485,
+        margin:'0px 20px',
+        height: 485
+    },
+    descriptionFather:{
+        fontSize:10,
+        fontFamily: 'Heebo',
+        maxWidth:458
+    },
+    price:{
+        fontWeight: 900,
+        color:'grey',
+        fontFamily: 'Itim',
+        fontSize: 40,
+        margin:0
+    },
+    oferta:{
+        color:'#F9C312',
+        fontFamily: 'Roboto Mono',
+        fontSize:20
+    },
+    littleDescription:{
+        fontSize:14
+    },
+    inputCantidad:{
+        width:50,
+        height:25,
+        border:'1px solid grey',
+        marginBottom:10,
+        marginTop:0,
+        textAlign:'center'
+    },
+    textCantidad:{
+        color: 'black',
+        fontSize:20,
+        marginBottom:0
+    },
+    buttonColor:{
+        backgroundColor:'#F9C312'
+    }
+    
+}
+
+export default style
