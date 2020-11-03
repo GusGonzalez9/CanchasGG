9242c1a2ecf2c74ab6e4890851cb283b56b9e45b
gustavo <gustavo4111997@gmail.com>
2020-11-03 15:30:19 -0300

    front

---
+++ front/src/components/NavBar/NavbarStyle.js
@@ -0,0 +1,32 @@
+import { green } from "@material-ui/core/colors"
+
+ const style ={
+  Item :{
+    margin: 18,
+    marginBottom: 0,
+    marginTop: 0,
+    listStyle: 'none',
+    color: "white",
+    fontFamily: 'Roboto , sans-serif',
+  },
+  NavBarList :{
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  AppBar:{
+    backgroundColor : '#012574',
+  },
+  button:{
+    backgroundColor:"#F9C312",
+    margin : 5,
+  },
+  AppBar2:{
+    backgroundColor:"#01277A"
+  }
+}
+
+export default style
+
+
+ 
