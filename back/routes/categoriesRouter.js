41d1935b545e95e06d4901b752d09591ee0c9ac3
pnestevez <pnestevez@gmail.com>
2020-11-04 14:37:29 -0300

    Product routes: done

--- back/routes/categoriesRouter.js
+++ back/routes/categoriesRouter.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const {getCategories, addCategory, deleteCategory, getCategory} = require('./controllers/categories-controllers')
+const {
+    getCategories,
+    addCategory,
+    deleteCategory,
+    getSingleCategory,
+    updateCategory
+} = require('./controllers/categories-controllers')
 
 router.get("/", getCategories);
 router.post("/", addCategory);
-router.get("/:id", getCategory);
+router.get("/:id", getSingleCategory);
+router.put("/:id", updateCategory);
 router.delete("/:id", deleteCategory);
 
 module.exports = router;
